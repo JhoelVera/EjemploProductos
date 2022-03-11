7016b22378ed975580420dc3857ff08386d62cb6
JhoelVera <101132565+JhoelVera@users.noreply.github.com>
2022-03-10 22:45:10 -0500

    clase persona

---
+++ EjemploProductos/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploProductos
+{
+    class Persona
+    {
+        public int num1;
+        //prueba de repositorio
+    }
+}
